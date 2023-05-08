df7f2aa9e2713576ad0fb0b1ebc72d01af87848f
mrmattyboy1 <132430489+mrmattyboy1@users.noreply.github.com>
2023-05-08 17:04:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "exmp5a32" {
+resource "null_resource" "exmp5a322" {
   provisioner "local-exec" {
     command = "sleep 10; echo HI"
     interpreter = ["bash", "-c"]
@@ -6,28 +6,3 @@ resource "null_resource" "exmp5a32" {
 }
 
 
-resource "aws_security_group" "securityissue" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = "vpc-1234567890abcdef0"
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls_from_internet"
-  }
-}
